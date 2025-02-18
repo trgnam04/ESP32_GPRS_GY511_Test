584622d9edf92e3b53b35cd013e39fb62c0201f8
unknown <trungnamtbcm123@gmail.com>
2025-02-18 21:40:57 +0700

    fix reboot

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,13 @@ void displayInfo();
 char information[20];
 void Task_Display(void* pvParameters);
 void Task_Serial(void* pvParameters);
+void convertData(void);
+
+
+double lat;
+double lng;
+char buffer[20];
+
 void setup() {
   // put your setup code here, to run once:
   xMutex = xSemaphoreCreateBinary();
@@ -19,66 +26,76 @@ void setup() {
   }
   Serial.begin(115200);
   hardware.begin(9600);
-  xTaskCreatePinnedToCore(Task_Display, "Task_Test_Mag_Sensor", 10000, NULL, 1, NULL, 1);
-  xTaskCreatePinnedToCore(Task_Serial, "Task_Test_LCD", 10000, NULL, 2, NULL, 0);
+  xTaskCreatePinnedToCore(Task_Display, "Task_Test_Mag_Sensor", 4096, NULL, 1, NULL, 1);
+  xTaskCreatePinnedToCore(Task_Serial, "Task_Test_LCD", 4096, NULL, 1, NULL, 0);
 }
 void loop()
 {
 }
+
+void convertData(void){
+  snprintf(buffer, 20, "%.6f;%.6f", lat, lng);
+}
+
 void displayInfo()
 {
-  Serial.print(F("Location: ")); 
-  if (gps.location.isValid())
-  {
-    Serial.print(gps.location.lat(), 6);
-    Serial.print(F(","));
-    Serial.print(gps.location.lng(), 6);
-  }
-  else
-  {
-    Serial.print(F("INVALID"));
-  }
+  convertData();
+  Serial.print(buffer);
+  // Serial.print(F("Location: ")); 
+  // if (gps.location.isValid())
+  // {
+  //   lat = gps.location.lat();
+  //   lng = gps.location.lng();
+  //   Serial.print(lat, 6);
+  //   Serial.print(F(","));
+  //   Serial.print(lng, 6);
+    
+  // }
+  // else
+  // {
+  //   Serial.print(F("INVALID"));
+  // }
 
-  Serial.print(F("  Date/Time: "));
-  if (gps.date.isValid())
-  {
-    Serial.print(gps.date.month());
-    Serial.print(F("/"));
-    Serial.print(gps.date.day());
-    Serial.print(F("/"));
-    Serial.print(gps.date.year());
-  }
-  else
-  {
-    Serial.print(F("INVALID"));
-  }
+  // Serial.print(F("  Date/Time: "));
+  // if (gps.date.isValid())
+  // {
+  //   Serial.print(gps.date.month());
+  //   Serial.print(F("/"));
+  //   Serial.print(gps.date.day());
+  //   Serial.print(F("/"));
+  //   Serial.print(gps.date.year());
+  // }
+  // else
+  // {
+  //   Serial.print(F("INVALID"));
+  // }
 
-  Serial.print(F(" "));
-  if (gps.time.isValid())
-  {
-    if (gps.time.hour() < 10) Serial.print(F("0"));
-    Serial.print(gps.time.hour());
-    Serial.print(F(":"));
-    if (gps.time.minute() < 10) Serial.print(F("0"));
-    Serial.print(gps.time.minute());
-    Serial.print(F(":"));
-    if (gps.time.second() < 10) Serial.print(F("0"));
-    Serial.print(gps.time.second());
-    Serial.print(F("."));
-    if (gps.time.centisecond() < 10) Serial.print(F("0"));
-    Serial.print(gps.time.centisecond());
-  }
-  else
-  {
-    Serial.print(F("INVALID"));
-  }
+  // Serial.print(F(" "));
+  // if (gps.time.isValid())
+  // {
+  //   if (gps.time.hour() < 10) Serial.print(F("0"));
+  //   Serial.print(gps.time.hour());
+  //   Serial.print(F(":"));
+  //   if (gps.time.minute() < 10) Serial.print(F("0"));
+  //   Serial.print(gps.time.minute());
+  //   Serial.print(F(":"));
+  //   if (gps.time.second() < 10) Serial.print(F("0"));
+  //   Serial.print(gps.time.second());
+  //   Serial.print(F("."));
+  //   if (gps.time.centisecond() < 10) Serial.print(F("0"));
+  //   Serial.print(gps.time.centisecond());
+  // }
+  // else
+  // {
+  //   Serial.print(F("INVALID"));
+  // }
 
   Serial.println();
 }
 void Task_Display(void* pvParameters)
 {
     while(1){                
-        Serial.println(millis());
+        Serial.print(millis());
         /* Delay before the next sample */
         if(xSemaphoreTake(xMutex, portMAX_DELAY)){
           displayInfo();
@@ -95,8 +112,8 @@ void Task_Serial(void* pvParameters)
         if(xSemaphoreTake(xMutex, portMAX_DELAY)){
           gps.encode(hardware.read());
           xSemaphoreGive(xMutex);
-        }
-          
+        }          
       }
+      vTaskDelay(10);
     }
 }
