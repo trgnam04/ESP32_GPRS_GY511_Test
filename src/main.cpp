415ca40854b0ad66c4960bec47ee033b72507f47
Tran Xuan Nam <tranxuannam02@gmail.com>
2025-02-19 10:13:09 +0700

    add http_get function

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_LSM303_U.h>
+#include <HTTPClient.h>
 
+#include <SPI.h>
+#include <HttpClient.h>
+#include <WiFi.h>
 
 #define SDA GPIO_NUM_21
 #define SCL GPIO_NUM_22
@@ -25,39 +29,131 @@ void displaySensorDetails(void)
   delay(500);
 }
 
-void setup(void)
-{
+WiFiClient wifi;
+HttpClient http(wifi);
+const char* ssid = "v";
+const char* password = "12345678";
+// Name of the server we want to connect to
+const char kHostname[] = "ems.thebestits.vn";
+const char kPath[] = "/Value/GetData"; // ?stationcode=K13_TEST&data=kaka_nam
 
-  Serial.begin(9600);
+// Number of milliseconds to wait without receiving any data before we give up
+const int kNetworkTimeout = 30 * 1000;
+// Number of milliseconds to wait if no data is available before trying again
+const int kNetworkDelay = 1000;
+
+void lsm303_init(){
   Wire.begin(SDA, SCL);
   Serial.println("Accelerometer Test"); Serial.println("");
-
-  /* Initialise the sensor */
   if(!mag.begin())
   {
     /* There was a problem detecting the ADXL345 ... check your connections */
     Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
     while(1);
-  }
-
-  /* Display some basic information on this sensor */
+  };
   displaySensorDetails();
+};
+
+void wifi_init(){
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println("Connected!");
+};
+
+int http_get(const char * hostname, const char * path, char * data, char * retData, int * retDataSize){
+  int err = 0;
+  *retDataSize = 0;
+  // path processing 
+  char newpath[200];
+  sprintf(newpath, "%s?stationcode=K13_TEST&data=%s", path, data);
+  err = http.get(hostname, newpath);
+  if (err == 0) {
+    Serial.println("start request");
+    err = http.responseStatusCode();
+    if (err >= 0) {
+      Serial.print("Status code: ");
+      Serial.println(err);
+      int bodyLen;
+      err = http.skipResponseHeaders();
+      if (err >= 0) {
+        bodyLen = http.contentLength();
+        // *size = bodyLen;
+        Serial.print("Content length is: ");
+        Serial.println(bodyLen);
+        Serial.println();
+        Serial.println("Body returned follows:");
+
+        unsigned long timeoutStart = millis();
+
+        while ((http.connected() || http.available()) &&
+               ((millis() - timeoutStart) < kNetworkTimeout)) {
+          if (http.available()) {
+            retData[(*retDataSize)++] = http.read();
+            bodyLen--;
+            timeoutStart = millis();
+          } else {
+            delay(kNetworkDelay);
+          }
+        }
+        Serial.println(); // Add a newline after the body
+
+      } else {
+        Serial.print("Failed to skip response headers: ");
+        Serial.println(err);
+        return -1;
+      }
+    } else {
+      Serial.print("Getting response failed: ");
+      Serial.println(err);
+      return -1;
+    }
+  } else {
+    Serial.print("Connect failed: ");
+    Serial.println(err);
+    return -1;
+  };
+  retData[*retDataSize] = 0;
+  return 0;
+};
+
+void setup(void)
+{
+  Serial.begin(9600);
+  // LSM303
+  lsm303_init();
+  // WIFI
+  wifi_init();
 }
 
 void loop(void)
 {
   /* Get a new sensor event */
-  sensors_event_t event;
-  mag.getEvent(&event);
+  // sensors_event_t event;
+  // mag.getEvent(&event);
 
   /* Display the results (acceleration is measured in m/s^2) */
 //   Serial.print("X: "); Serial.print(event.acceleration.x); Serial.print("  ");
 //   Serial.print("Y: "); Serial.print(event.acceleration.y); Serial.print("  ");
 //   Serial.print("Z: "); Serial.print(event.acceleration.z); Serial.print("  ");Serial.println("m/s^2 ");
 
-  Serial.print("__X: "); Serial.print(event.magnetic.x); Serial.print("  ");
-  Serial.print("__Y: "); Serial.print(event.magnetic.y); Serial.print("  ");
-  Serial.print("__Z: "); Serial.print(event.magnetic.z); Serial.print("  ");Serial.println("Gauss ");
+  // Serial.print("__X: "); Serial.print(event.magnetic.x); Serial.print("  ");
+  // Serial.print("__Y: "); Serial.print(event.magnetic.y); Serial.print("  ");
+  // Serial.print("__Z: "); Serial.print(event.magnetic.z); Serial.print("  ");Serial.println("Gauss ");
   /* Delay before the next sample */
-  delay(50);
+  // delay(2000);
+  // ****************************************************************************************************
+  char data[] = "bkitk22";
+  char retData[100];
+  int retDataSize;
+
+  int err = http_get(kHostname, kPath, data, retData, &retDataSize);
+  
+  Serial.println(retData);
+  http.stop();
+
+  while (1);
 }
