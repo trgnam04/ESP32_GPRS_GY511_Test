8a6d6742fd4d98ec57e829d80bc0d09d28aaa2f4
unknown <trungnamtbcm123@gmail.com>
2025-02-28 17:23:58 +0700

    test synchronize data between tasks

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,13 @@
 
 U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 
+typedef struct {
+    uint16_t trip_number = 0;
+    uint16_t route_id = 0;
+} data_t;
+
+data_t Data;
+
 void drawStaticMenu(void){
     u8g2.clearBuffer();    
     u8g2.setFont(u8g2_font_6x12_tf);
@@ -17,55 +24,8 @@ void drawStaticMenu(void){
     u8g2.sendBuffer();
 }
 
-void drawArrowSelect(uint8_t state){  
-  u8g2.setDrawColor(0); 
-  u8g2.drawBox(3, 25, 10, 30);
-  u8g2.setDrawColor(1);
-  int arrowY = (state == 0) ? 50 : 35;
-  u8g2.drawStr(5, arrowY, ">");
-  u8g2.sendBuffer();
-}
 
-void displaySubPage2(void){
-    // Kích thước bảng tối ưu
-    int rowHeight = 11;   // Chiều cao mỗi hàng (để cân đối)
-    int paramWidth = 30;  // Chiều rộng cột "Param" (Nhỏ hơn)
-    int valueWidth = 80;  // Chiều rộng cột "Value" (Lớn hơn)
-    int startX = 5;       // Lề trái bảng
-    int startY = 10;      // Lề trên bảng
-  
-    // Vẽ các đường ngang (chia 5 hàng)
-    for (int i = 0; i <= 5; i++) {
-      u8g2.drawHLine(startX, startY + (i * rowHeight), paramWidth + valueWidth);
-    }
-  
-    // Vẽ đường dọc chia 2 cột
-    u8g2.drawVLine(startX + paramWidth, startY, rowHeight * 5);
-  
-    // Chọn font chữ nhỏ để hiển thị tối ưu
-    u8g2.setFont(u8g2_font_6x10_tf);
-  
-    // Hiển thị tiêu đề cột
-    u8g2.drawStr(startX + 5, startY - 2, "P");  // Cột 1 (Param nhỏ gọn)
-    u8g2.drawStr(startX + paramWidth + 5, startY - 2, "Value");  // Cột 2
-  
-    // Hiển thị các thông số
-    const char* params[] = {"ax", "ay", "az", "lat", "lng"};
-    const char* values[] = {"1.23", "-0.98", "0.50", "10.1234", "106.5678"};
-  
-    for (int i = 0; i < 5; i++) {
-      u8g2.drawStr(startX + 5, startY + (i + 1) * rowHeight - 2, params[i]);
-      u8g2.drawStr(startX + paramWidth + 5, startY + (i + 1) * rowHeight - 2, values[i]);
-    }
-  
-    // Gửi buffer lên màn hình
-    u8g2.sendBuffer();
-}
-
-int tripNumber = 0;
-int routeID = 0;
-
-void updateValues() {
+void updateValues(data_t tData) {
     // Xóa giá trị cũ
     u8g2.setDrawColor(0);
     u8g2.drawBox(100, 25, 30, 40);  // Xóa vùng giá trị số
@@ -74,83 +34,90 @@ void updateValues() {
     u8g2.setDrawColor(1);
     char buffer[10];
     
-    sprintf(buffer, "%d", tripNumber);
+    sprintf(buffer, "%d", tData.trip_number);
     u8g2.drawStr(100, 35, buffer);
   
-    sprintf(buffer, "%d", routeID);
+    sprintf(buffer, "%d", tData.route_id);
     u8g2.drawStr(100, 50, buffer);
   
     u8g2.sendBuffer();
 }
 
-uint8_t press;
-uint8_t longpress;
-uint8_t inc;
-uint8_t dec;
 
-void updateButtonState(void){
-    press = isPressed();
-    longpress = isLongPressed();
-    inc = isIncrese();
-    dec = isDecrease();
+TaskHandle_t readRotaryEncTask_handle;
+TaskHandle_t DisplayProcessTask_handle;
+TaskHandle_t DataProcessTask_handle;
+
+QueueHandle_t DataQueue;
+QueueHandle_t Data2Queue;
+
+
+void readRotaryEncTask(void* vParameters){
+    RotaryEncoder_setup();
+    uint8_t state = 0;
+
+    for(;;){
+        switch(state){
+            case 0:{
+                if(isLongPressed()){
+                    vTaskResume(DataProcessTask_handle);
+                    state = 1;
+                }
+                break;
+            }
+            case 1:{
+                if(isLongPressed()){
+                    vTaskSuspend(DataProcessTask_handle);
+                    state = 0;
+                }
+                break;
+            }
+        }                
+        RotaryEncoder_loop();
+        vTaskDelay(TIME_READ);
+    }
+
 }
 
-uint8_t state = 3;
+void DisplayProcessTask(void* vParameters){
+    Wire.begin(SDA, SCK);
+    u8g2.begin();    
+    data_t ReceivedData;
+    drawStaticMenu();
+
+    for(;;){                 
+        xQueueReceive(DataQueue, &ReceivedData, portMAX_DELAY);
+        updateValues(ReceivedData);
+        vTaskDelay(100);
+    }
+
+}
+
+void DataProcessTask(void* vParameters){
+    for(;;){
+        Data.route_id ++;
+        Data.trip_number ++;
+        xQueueSendToBack(DataQueue, &Data, portMAX_DELAY);
+        vTaskDelay(1000);
+    }
+}
+
 
 void setup() {  
-    Wire.begin(SDA, SCK);
-    u8g2.begin();
-    RotaryEncoder_setup();
-    drawStaticMenu();        
+    Serial.begin(9600);
+                
+    DataQueue = xQueueCreate(5, sizeof(data_t));
+    Data2Queue = xQueueCreate(5, sizeof(data_t));
+    
+    xTaskCreatePinnedToCore(DisplayProcessTask, "Display Process", (1024 * 2), NULL, 1, &DisplayProcessTask_handle, 0);
+
+    xTaskCreatePinnedToCore(DataProcessTask, "Datalay Process", (1024 * 1), NULL, 1, &DataProcessTask_handle, 0);
+    vTaskSuspend(DataProcessTask_handle);
+
+    xTaskCreatePinnedToCore(readRotaryEncTask, "Read Rotary Encoder", (1024 * 1), NULL, 1, &readRotaryEncTask_handle, 0);
+    
+
 }
 
 void loop() {        
-    switch(state){
-        case 3:{
-            if(1){
-                drawArrowSelect(1);
-                state = 1;
-            }
-            break;
-        }
-        case 1:{
-            if(isDecrease()){                
-                tripNumber--;
-                break;    
-            }
-            if(isIncrese()){                
-                tripNumber++;
-                break;
-            }
-            if(isPressed()){                
-                drawArrowSelect(0);
-                state = 0;
-            }
-            break;
-        }
-        case 0:{
-            if(isDecrease()){            
-                routeID--;
-                break;
-            }
-            if(isIncrese()){                
-                routeID++;
-                break;
-            }
-            if(isPressed()){                
-                drawArrowSelect(1);
-                state = 1;
-            }
-            break;
-        }
-    }    
-    // if(isIncrese()){
-    //     tripNumber++;
-    // }
-    // if(isDecrease()){
-    //     tripNumber--;
-    // }        
-    updateValues();
-    RotaryEncoder_loop();    
-    delay(5);
 }    
