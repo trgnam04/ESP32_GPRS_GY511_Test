031c5016a86198a63b7e163d91c61ce07b64b9ab
Tran Xuan Nam <tranxuannam02@gmail.com>
2025-03-23 11:08:19 +0700

    detect stop and calc velocity

--- src/main.cpp
+++ src/main.cpp
@@ -14,9 +14,13 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
+#include <U8g2lib.h>
+
 
 #define MS_TO_KMH 18.0f / 5.0f
 #define KMH_TO_MS 5.0f / 18.0f
+
+U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/U8X8_PIN_NONE);
 
 // ================================================================ CONST
 const int RXPin = 17, TXPin = 18;
@@ -56,36 +60,18 @@ QueueHandle_t gpsQueue;
 WiFiClient espClient;
 Arduino_MQTT_Client mqttClient(espClient);
 Server_Side_RPC<MAX_RPC_SUBSCRIPTIONS, MAX_RPC_RESPONSE> rpc;
-const std::array<IAPI_Implementation*, 1U> apis = {
-    &rpc
-};
+const std::array<IAPI_Implementation *, 1U> apis = {
+    &rpc};
 ThingsBoard tb(mqttClient, MAX_MESSAGE_RECEIVE_SIZE, MAX_MESSAGE_SEND_SIZE, Default_Max_Stack_Size, apis);
-
-
 
 // =================================================================== Funciton prototype
 void InitWiFi();
 bool reconnect();
 void sendTelemetryData();
-void printEvent(sensors_event_t* event);
+void printEvent(sensors_event_t *event);
+float calculate_std_deviation(float arr[], int n);
 // ==================================================================================== SETUP
-void setup(void)
-{
-  Serial.begin(9600);
-  // InitWiFi();
-  ss.begin(GPSBaud);
-  while (!Serial)
-    delay(10); // will pause Zero, Leonardo, etc until serial console opens
 
-    Serial.println("Orientation Sensor Test"); Serial.println("");
-    if (!bno.begin())
-    {
-      /* There was a problem detecting the BNO055 ... check your connections */
-      Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
-      while (1);
-    };
-    delay(1000);
-}
 sensors_event_t a, g, temp;
 
 //================================================ variable for data
@@ -110,44 +96,135 @@ unsigned int count_for_mean = 0;
 float roll, pitch; // after complementary with ax, ay, az
 
 float ax_filtered = 0.0, ay_filtered = 0.0, az_filtered = 0.0;
-float ax_linear = 0.0f, ay_linear=0.0f;
+float ax_linear = 0.0f, ay_linear = 0.0f;
 
 float ax_temp = 0.0f, ay_temp = 0.0f, az_temp = 0.0f;
 float gx_temp = 0.0f, gy_temp = 0.0f, gz_temp = 0.0f;
+
+void displayMessage(const char *message) {
+  u8g2.firstPage();
+  do {
+      u8g2.setFont(u8g2_font_ncenB08_tr);
+      u8g2.setCursor(10, 30);
+      u8g2.print(message);
+  } while (u8g2.nextPage());
+}
+
+void displayCalibrationStatus() {
+  uint8_t sys, gyro, accel, mag;
+  bno.getCalibration(&sys, &gyro, &accel, &mag);
+  
+  u8g2.firstPage();
+  do {
+      u8g2.setFont(u8g2_font_6x12_tf);
+      u8g2.setCursor(25, 15);
+      u8g2.print("Calibrating...");
+      u8g2.setCursor(10, 30);
+      u8g2.print("Sys: "); u8g2.print(sys);
+      u8g2.setCursor(10, 40);
+      u8g2.print("Gyro: "); u8g2.print(gyro); 
+      u8g2.setCursor(10, 50);
+      u8g2.print("Accel: "); u8g2.print(accel);
+      u8g2.setCursor(10, 60);
+      u8g2.print("Mag: "); u8g2.print(mag);
+  } while (u8g2.nextPage());
+}
+
+void displayVelocity(float Vx, float Vy) {
+  u8g2.firstPage();
+  do {
+      u8g2.setFont(u8g2_font_ncenB08_tr);
+      u8g2.setCursor(10, 20);
+      u8g2.print("Vx: ");
+      u8g2.print(Vx * MS_TO_KMH);
+      u8g2.print(" Km/h");
+      
+      u8g2.setCursor(10, 40);
+      u8g2.print("Vy: ");
+      u8g2.print(Vy * MS_TO_KMH);
+      u8g2.print(" Km/h");
+  } while (u8g2.nextPage());
+}
+
+void setup() {
+  u8g2.begin();
+  u8g2.enableUTF8Print();
+  Serial.begin(9600);
+  
+  if (!bno.begin()) {
+      Serial.println("Không tìm thấy BNO055, kiểm tra kết nối!");
+      while (1);
+  }
+  
+  
+  // Hiển thị trạng thái hiệu chỉnh
+  while (true) {
+      uint8_t sys, gyro, accel, mag;
+      bno.getCalibration(&sys, &gyro, &accel, &mag);
+      displayCalibrationStatus();
+      
+      if (sys == 3 && gyro == 3 && accel == 3 && mag == 3) {
+          break;
+      }
+      delay(500);
+  }
+  
+  displayMessage("Calibration Complete!");
+  delay(2000);
+}
 
 
 // TODO
 // ax_filter, ay_filter, lat, lon, deltaT, real_velocity
 // ==================================================================================== LOOP
+float arrDataX[10];
+float arrDataY[10];
+int arr_countX = 0;
+int arr_countY = 0;
+int stop_count = 0;
+int moving_count = 0;
+float vx = 0.0f, vy = 0.0f;
 void loop()
 {
   // Get data
-  sensors_event_t orientationData , angVelocityData , linearAccelData, magnetometerData, accelerometerData, gravityData;
-  bno.getEvent(&orientationData, Adafruit_BNO055::VECTOR_EULER);
-  bno.getEvent(&angVelocityData, Adafruit_BNO055::VECTOR_GYROSCOPE);
+  sensors_event_t orientationData, angVelocityData, linearAccelData, magnetometerData, accelerometerData, gravityData;
   bno.getEvent(&linearAccelData, Adafruit_BNO055::VECTOR_LINEARACCEL);
-  bno.getEvent(&magnetometerData, Adafruit_BNO055::VECTOR_MAGNETOMETER);
-  bno.getEvent(&accelerometerData, Adafruit_BNO055::VECTOR_ACCELEROMETER);
-  bno.getEvent(&gravityData, Adafruit_BNO055::VECTOR_GRAVITY);
-
-  printEvent(&orientationData);
-  printEvent(&angVelocityData);
-  printEvent(&linearAccelData);
-  printEvent(&magnetometerData);
-  printEvent(&accelerometerData);
-  printEvent(&gravityData);
 
   uint8_t system, gyro, accel, mag = 0;
-  bno.getCalibration(&system, &gyro, &accel, &mag);
-  Serial.println();
-  Serial.print("Calibration: Sys=");
-  Serial.print(system);
-  Serial.print(" Gyro=");
-  Serial.print(gyro);
-  Serial.print(" Accel=");
-  Serial.print(accel);
-  Serial.print(" Mag=");
-  Serial.println(mag);
+  // bno.getCalibration(&system, &gyro, &accel, &mag);
+
+    float stdX = 0.0f, stdY = 0.0f;
+    arrDataX[arr_countX++] = linearAccelData.acceleration.x;
+    arrDataY[arr_countX++] = linearAccelData.acceleration.y;
+
+    vx += linearAccelData.acceleration.x * 0.1;
+    vy += linearAccelData.acceleration.y * 0.1;
+    Serial.print(vx); Serial.print("\t"); Serial.println(vy);
+    if (arr_countX == 10)
+    {
+
+      stdX = calculate_std_deviation(arrDataX, 10);
+      stdY = calculate_std_deviation(arrDataY, 10);
+
+      if (stdX < 0.25 && stdY < 0.21) stop_count++;
+      else moving_count++;
+      if (stop_count == 3)
+      {
+        Serial.println("STOP");
+        vx = 0.0f;
+        vy = 0.0f;
+        stop_count = 0;
+      }
+      if(moving_count == 3)
+      {
+        Serial.println("MOVING");
+        moving_count = 0;
+      }
+      arr_countX = 0;
+    }
+
+  displayVelocity(vx, vy);
+
   delay(BNO055_SAMPLERATE_DELAY_MS);
   // currentMillis = millis();
 
@@ -219,7 +296,6 @@ void loop()
   //   ay_filtered = beta * ay_filtered + (1.0f - beta) * ay_linear;
   //   az_filtered = beta * az_filtered + (1.0f - beta) * az_linear;
 
-
   //   if (gps.location.isUpdated()) // Nếu có dữ liệu mới
   //   {
   //     Serial.print("Latitude: ");
@@ -254,9 +330,24 @@ void loop()
   // tb.loop();
 };
 
+float calculate_std_deviation(float arr[], int n)
+{
+  float sum = 0.0f;
+  for (int i = 0; i < n; i++)
+  {
+    sum += arr[i];
+  }
+  float mean = sum / n;
+  float sum_deviation = 0.0f;
+  for (int i = 0; i < n; i++)
+  {
+    sum_deviation += (arr[i] - mean) * (arr[i] - mean);
+  }
+  return sqrt(sum_deviation / n);
+}
 
-
-void sendTelemetryData(){
+void sendTelemetryData()
+{
   data[COLLECTOR_KEY_LNG] = lng;
   data[COLLECTOR_KEY_LAT] = lat;
   data[COLLECTOR_KEY_ACCEL_X] = ax_linear;
@@ -266,81 +357,96 @@ void sendTelemetryData(){
   tb.sendTelemetryJson(data, data_size);
 }
 
-void InitWiFi() {
+void InitWiFi()
+{
   Serial.println("Connecting to AP ...");
   // Attempting to establish a connection to the given WiFi network
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
   Serial.println("Connected to AP");
 };
 
-bool reconnect() {
+bool reconnect()
+{
   const wl_status_t status = WiFi.status();
-  if (status == WL_CONNECTED) {
+  if (status == WL_CONNECTED)
+  {
     return true;
   }
   InitWiFi();
   return true;
 };
 
-
-void printEvent(sensors_event_t* event) {
-  double x = -1000000, y = -1000000 , z = -1000000; //dumb values, easy to spot problem
-  if (event->type == SENSOR_TYPE_ACCELEROMETER) {
-    Serial.print("Accl:");
+void printEvent(sensors_event_t *event)
+{
+  double x = -1000000, y = -1000000, z = -1000000; // dumb values, easy to spot problem
+  if (event->type == SENSOR_TYPE_ACCELEROMETER)
+  {
+    // Serial.print("Accl:");
     x = event->acceleration.x;
     y = event->acceleration.y;
     z = event->acceleration.z;
   }
-  else if (event->type == SENSOR_TYPE_ORIENTATION) {
+  else if (event->type == SENSOR_TYPE_ORIENTATION)
+  {
     Serial.print("Orient:");
     x = event->orientation.x;
     y = event->orientation.y;
     z = event->orientation.z;
   }
-  else if (event->type == SENSOR_TYPE_MAGNETIC_FIELD) {
+  else if (event->type == SENSOR_TYPE_MAGNETIC_FIELD)
+  {
     Serial.print("Mag:");
     x = event->magnetic.x;
     y = event->magnetic.y;
     z = event->magnetic.z;
   }
-  else if (event->type == SENSOR_TYPE_GYROSCOPE) {
+  else if (event->type == SENSOR_TYPE_GYROSCOPE)
+  {
     Serial.print("Gyro:");
     x = event->gyro.x;
     y = event->gyro.y;
     z = event->gyro.z;
   }
-  else if (event->type == SENSOR_TYPE_ROTATION_VECTOR) {
+  else if (event->type == SENSOR_TYPE_ROTATION_VECTOR)
+  {
     Serial.print("Rot:");
     x = event->gyro.x;
     y = event->gyro.y;
     z = event->gyro.z;
   }
-  else if (event->type == SENSOR_TYPE_LINEAR_ACCELERATION) {
-    Serial.print("Linear:");
+  else if (event->type == SENSOR_TYPE_LINEAR_ACCELERATION)
+  {
+    // Serial.print("Linear:");
     x = event->acceleration.x;
     y = event->acceleration.y;
     z = event->acceleration.z;
   }
-  else if (event->type == SENSOR_TYPE_GRAVITY) {
+  else if (event->type == SENSOR_TYPE_GRAVITY)
+  {
     Serial.print("Gravity:");
     x = event->acceleration.x;
     y = event->acceleration.y;
     z = event->acceleration.z;
   }
-  else {
+  else
+  {
     Serial.print("Unk:");
   }
 
-  Serial.print("\tx= ");
-  Serial.print(x);
-  Serial.print(" |\ty= ");
-  Serial.print(y);
-  Serial.print(" |\tz= ");
-  Serial.println(z);
+  // Serial.print("\tx= ");
+  // Serial.print(x);
+  // Serial.print(" |\ty= ");
+  // Serial.print(y);
+  // Serial.print(" |\tz= ");
+  // Serial.println(z);
+  Serial.print(x, 6);
+  Serial.print("\t");
+  Serial.print(y, 6);
+  Serial.print("\t");
+  Serial.println(z, 6);
 }
-
-  
